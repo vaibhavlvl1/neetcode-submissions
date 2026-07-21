70b1c4229fa8ac81536961349ddfedae011c00c9
vaibhavlvl1 <110812464+vaibhavlvl1@users.noreply.github.com>
2026-07-21 16:59:27 +0530

    Add: is-anagram - submission-7

---
+++ Data Structures & Algorithms/is-anagram/submission-7.cs
@@ -0,0 +1,30 @@
+public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(s.Length != t.Length){
+            return false;
+        }
+        Dictionary<char, int> D = new Dictionary<char, int>();
+
+        foreach (char letter in s) {
+            if (!D.ContainsKey(letter)) {
+                D[letter] = 1;
+            }
+            else if (D.ContainsKey(letter)) {
+                D[letter] = D[letter] + 1;
+            }
+        }
+
+        foreach (char letter in t) {
+            if(!D.ContainsKey(letter)){
+               return false;
+            }
+            D[letter] = D[letter] - 1;
+        }
+
+        if (D.ContainsValue(-1)) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+}
