19997f05b26c8ba5d0f54e6a686fdc584184f6f0
vaibhavlvl1 <110812464+vaibhavlvl1@users.noreply.github.com>
2026-07-21 16:34:50 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,21 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        D = {}
+
+        for char in s:
+            if D.get(char,0) == 0:
+                D[char] = 1
+            elif char in D:
+                D[char] = D.get(char, 0) + 1
+
+        for char in t:
+            D[char] = D.get(char, 0) - 1
+
+        for value in D.values():
+            if value != 0:
+                return False
+
+        return True
